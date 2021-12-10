b605421fc44c515dd2036a20403d241ac54795d7
Henry Feng <henryfeng12345@gmail.com>
2021-12-09 20:11:02 -0600

    push frontend for mint

---
+++ bbaf-nft-mint/src/components/Header/HeaderElements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const HeaderH1 = styled.h1``;
---
+++ bbaf-nft-mint/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeaderContainer, HeaderContent, HeaderH1 } from "./HeaderElements";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <HeaderH1>BBAF MINT</HeaderH1>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
